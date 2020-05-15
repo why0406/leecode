a8945e848652943ef49a15e019e28c44853d6142
why0406 <hzzdream@163.com>
2020-05-15 22:16:04 +0800

    modify bubble_sort.c

--- algorithm/bubble_sort.c
+++ algorithm/bubble_sort.c
@@ -19,15 +19,9 @@ void BubbleSort(int *array, int width)
 {
     int i, j;
     for(i = 0; i < width; i++) {
-<<<<<<< 8724d5824f5127f2657e0b153becda3ee20338b6
-        for(j = 0; j < width-i-1; j++) {
-            if(array[j] > array[j+1]) {
-                swap(&array[j], &array[j+1]);
-=======
         for(j = 0; j < width-i-1; j++) { //注意内循环次数要减一，否则最长长度加一以后就溢出了
             if(array[j] > array[j+1]) {
                 swap(&array[j], &array[j+1]); //传递地址
->>>>>>> add bubble_sort.c
             }
         }
     }
@@ -39,11 +33,7 @@ int main(int argc, char *argv[])
     int array[6] = {-1,0,1,2,-1,-4};
     int width = sizeof(array)/sizeof(int);
 
-<<<<<<< 8724d5824f5127f2657e0b153becda3ee20338b6
-    BubbleSort(array, width);
-=======
     BubbleSort(array, width);//数组名传参，数组首（元素）地址
->>>>>>> add bubble_sort.c
     PrintArray(array, width);
     return 0;
 }
